380276974bb59af4888664dbb9331dea062087cd
Your Name <you@example.com>
2022-09-24 23:12:50 -0700

    update in query

--- bank.js
+++ bank.js
@@ -1,8 +1,12 @@
-module.exports.sql = require('sqlite3').verbose();
-module.exports.db = new this.sql.Database('./db/bank.db', this.sql.OPEN_READWRITE, (err) => {
-    if (err) return console.log(err.message);
-    console.log("db connected");
-});
+module.exports.sql = require("sqlite3").verbose();
+module.exports.db = new this.sql.Database(
+    "./db/bank.db",
+    this.sql.OPEN_READWRITE,
+    (err) => {
+        if (err) return console.log(err.message);
+        console.log("db connected");
+    }
+);
 module.exports.add = (question) => {
     this.db.run(`INSERT INTO question (
         text,
@@ -23,17 +27,27 @@ module.exports.add = (question) => {
         "${question.type_a}",
         "${question.answer}"
     );
-    `)
-}
+    `);
+};
 module.exports.delete = (id) => {
-    this.db.run(`delete from question where id=${id}`)
-}
-module.exports.get = () => {
+    this.db.run(`delete from question where id=${id}`);
+};
+module.exports.get = (data) => {
 
-}
-module.exports.update = () => {
-
-}
-module.exports.exam = () => {
-
-}
+};
+module.exports.update = (id,data) => {
+    keys = [];
+    values = [];
+    for (const [key, value] of Object.entries(data)) {
+        keys[keys.length] = `${key}=?`;
+        values[values.length] = value;
+    }
+    keys = keys.toString();
+    values[values.length] = id;
+    this.db.run(`
+    UPDATE question
+        ${keys}
+        where id=? 
+        `,values);
+};
+module.exports.exam = () => {};
