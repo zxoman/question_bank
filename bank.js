62fda3fbb55edf9bbe52102b52a475a384e8655a
zxoman <zxo1890@gmail.com>
2022-11-04 13:29:36 +0200

    alll

--- bank.js
+++ bank.js
@@ -1,27 +1,26 @@
 module.exports.sql = require("sqlite3").verbose();
 module.exports.db = new this.sql.Database(
-    "./db/bank.db",
-    this.sql.OPEN_READWRITE,
-    (err) => {
-        if (err) return console.log(err.message);
-        console.log("db connected");
-    }
+  "./db/bank.db",
+  this.sql.OPEN_READWRITE,
+  (err) => {
+    if (err) return console.log(err.message);
+    console.log("db connected");
+  }
 );
 module.exports.add = (question) => {
-    values = [];
-    keys = [];
-    for (const [key, value] of Object.entries(question)) {
-        values[values.length] = value;
-        keys[keys.length] = key;
-
-
-    }
-    console.log(keys);
-    this.db.run(`INSERT INTO question (
-        1,
-        2,
-        3,
-        4,
+  values = [];
+  keys = [];
+  for (const [key, value] of Object.entries(question)) {
+    values[values.length] = value;
+    keys[keys.length] = key;
+  }
+  console.log(keys);
+  this.db.run(
+    `INSERT INTO question (
+        '1',
+        '2',
+        '3',
+        '4',
         class,
         term,
         unit,
@@ -31,10 +30,10 @@ module.exports.add = (question) => {
         text,
         answer
     ) VALUES(
-        ?,
-        ?,
-        ?,
-        ?,
+        "",
+        "",
+        "",
+        "",
         ?,
         ?,
         ?,
@@ -44,42 +43,104 @@ module.exports.add = (question) => {
         ?,
         ?
     );
-    `,values);
+    `,
+    values
+  );
 };
 module.exports.delete = (id) => {
-    this.db.run(`delete from question where id=${id}`);
+  this.db.run(`delete from question where id=${id}`);
 };
-module.exports.get = (data,limit=false,call) => {
-    keys = [];
-    values = [];
-    for (const [key, value] of Object.entries(data)) {
-        keys[keys.length] = `${key}=?`;
-        values[values.length] = value;
-    }
-    keys = keys.toString();
-    var limit_query = '' 
-    if (limit) {
-        limit_query = ` limit ${limit}`
-    }
-    if (keys.length>0) {
-        this.db.all(`select * from question where ${keys} ${limit_query}`,values,call)
-    }else{
-        this.db.all(`select * from question ${limit_query}`,call)
-    }
+module.exports.get = (data, limit = false, call) => {
+  keys = [];
+  values = [];
+  for (const [key, value] of Object.entries(data)) {
+    keys[keys.length] = `${key}=?`;
+    values[values.length] = value;
+  }
+  keys = keys.toString();
+  var limit_query = "";
+  if (limit) {
+    limit_query = ` limit ${limit}`;
+  }
+  if (keys.length > 0) {
+    this.db.all(
+      `select * from question where ${keys} ${limit_query}`,
+      values,
+      call
+    );
+  } else {
+    console.log(`select * from question ${limit_query}`);
+    this.db.all(`select * from question ${limit_query}`, call);
+  }
 };
-module.exports.update = (id,data) => {
-    keys = [];
-    values = [];
-    for (const [key, value] of Object.entries(data)) {
-        keys[keys.length] = `${key}=?`;
-        values[values.length] = value;
-    }
-    keys = keys.toString();
-    values[values.length] = id;
-    this.db.run(`
+module.exports.update = (id, data) => {
+  keys = [];
+  values = [];
+  for (const [key, value] of Object.entries(data)) {
+    keys[keys.length] = `${key}=?`;
+    values[values.length] = value;
+  }
+  keys = keys.toString();
+  values[values.length] = id;
+  this.db.run(
+    `
     UPDATE question set
         ${keys}
         where id=? 
-        `,values);
+        `,
+    values
+  );
 };
-module.exports.exam = () => {};
+module.exports.getwids = (data, call) => {
+  ids = `in(${"".padStart(data.length * 2, ",?").slice(1)})`;
+  this.db.all(`select * from question where id ${ids} `, data, call);
+};
+module.exports.shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+module.exports.exam = (
+  type,
+  obj = false,
+  ch = [],
+  ch_num = false,
+  con = [],
+  num
+) => {
+  console.log(ch);
+  ch = this.shuffle(ch).slice(0, ch_num);
+  con = this.shuffle(con);
+  qus = [];
+  console.log(ch);
+  this.getwids(ch, (e, d, s) => {
+    console.log(d.length);
+    for (const property in d) {
+        console.log(d[property])      
+        //qus[qus.length] = da;
+
+      }
+  });
+  if (con.length > 0) {
+    this.getwids(con, (e, d, s) => {
+      d.foreach((da) => {
+        qus[qus.length] = da;
+      });
+    });
+  }
+  console.log(this.shuffle(qus));
+};
--- socketOn.js
+++ socketOn.js
@@ -6,5 +6,20 @@ exports.connect = (socket,db=bank)=>{
             db.add(data)
             c.emit("add_questions","success")
         })
+        c.on("get_data",(data)=>{ 
+            db.get(data,false,(e,d,n)=>{
+                console.log(d.length);
+                c.emit("get_data",d);
+            })
+        })
+        c.on("edit",(data)=>{
+            db.update(data.id,data.info)
+            c.emit("edit","success")
+        })
+        c.on("del_question",(id)=>{
+            console.log('delete',id);
+            db.delete(id)
+            c.emit("del","success")
+        })
     })
 }
